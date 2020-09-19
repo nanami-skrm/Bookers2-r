6c3cba9791c4c32d930b9daab939272c0dc82e13
@nanami-skrm <skrm773@gmail.com>
2020-09-19 13:04:20 +0900

    [Add]部分テンプレート

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,16 @@
 class BooksController < ApplicationController
+
+	def create
+		@book = Book.new(books_params)
+		@book.save
+		redirect_to book_path(@book.id)
+	end
+
+	def show
+	end
+
+	private
+	def books_params
+		params.require(:book).permit(:title, :body)
+  	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_144603) do
+ActiveRecord::Schema.define(version: 2020_09_19_034601) do
 
   create_table "books", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "title"
+    t.text "body"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
