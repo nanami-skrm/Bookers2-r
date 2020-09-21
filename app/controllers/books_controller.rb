ea9cadeb13893f1480e0465aa52cede866b3b6f0
@nanami-skrm <skrm773@gmail.com>
2020-09-22 01:53:14 +0900

    [Add]refile

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,11 +2,15 @@ class BooksController < ApplicationController
 
 	def create
 		@book = Book.new(books_params)
+		@book.user = current_user
 		@book.save
-		redirect_to book_path(@book.id)
+		redirect_to book_path(@book)
 	end
 
 	def show
+		@book = Book.find(params[:id])
+		@user = @book.user
+		@new_book = Book.new
 	end
 
 	private
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'da803997c46964971d1dfa40056697e077abb4f2d1174c33e19dacb790d0855d0d15131eb974dd52b072c58429c3f87b5e8847a54487d8af62a05e6a0905ac23'
